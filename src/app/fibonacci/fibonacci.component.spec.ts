6f74843b6421f62a6e97703bcfe54a693602d0c6
Felix <felix.fatka@gmail.com>
2020-10-14 08:05:00 +0200

    Jest

---
+++ src/app/fibonacci/fibonacci.component.spec.ts
@@ -0,0 +1,55 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FibonacciComponent } from './fibonacci.component';
+
+describe('FibonacciComponent', () => {
+  let component: FibonacciComponent;
+  let fixture: ComponentFixture<FibonacciComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FibonacciComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FibonacciComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('should calculate fibonacci', () => {
+    it('for 0', () => {
+      expect(component.fib(0)).toBe(0);
+    });
+
+    it('for 1', () => {
+      expect(component.fib(1)).toBe(1);
+    });
+
+    it('for 2', () => {
+      expect(component.fib(2)).toBe(1);
+    });
+
+    it('for 3', () => {
+      expect(component.fib(3)).toBe(2);
+    });
+
+    it('for 4', () => {
+      expect(component.fib(4)).toBe(3);
+    });
+
+    it('for 5', () => {
+      expect(component.fib(5)).toBe(5);
+    });
+
+    it('for 6', () => {
+      expect(component.fib(6)).toBe(8);
+    });
+  });
+});
---
+++ src/app/fibonacci/fibonacci.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fibonacci',
+  templateUrl: './fibonacci.component.html',
+  styleUrls: ['./fibonacci.component.scss']
+})
+export class FibonacciComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  fib(n): number {
+    return 0;
+  }
+
+}
