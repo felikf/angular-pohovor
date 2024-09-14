712dbc53b4b8a5ec5d16692567f245b374d7c28a
felixfatka-ext97611 <felix.fatka@ext.csas.cz>
2024-09-14 10:33:13 +0200

    chore: angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,10 +8,6 @@ import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import { NgOptimizedImage } from '@angular/common';
 
-@NgModule({
-  declarations: [AppComponent, WeatherComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgOptimizedImage],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent, WeatherComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule, FormsModule, NgOptimizedImage], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
